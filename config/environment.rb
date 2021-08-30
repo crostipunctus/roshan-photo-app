71644b0521bb059284ccfab89726573553fea218
crostipunctus <63543350+crostipunctus@users.noreply.github.com>
2021-08-30 21:37:23 +0530

    changed env.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address => 'smtp.mailgun.org'
+  :port => '587',
+  :authentication => :plain,
+  :user_name => Rails.application.credentials.mailgun[:username],
+  :password => Rails.application.credentials.mailgun[:password],
+  :domain => 'http://roshan-photo-app.herokuapp.com',
+}
+ActionMailer::Base.delivery_method = :smtp
