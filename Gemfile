b58ceff2937220535a8ca098aaef786903e9cfa3
crostipunctus <63543350+crostipunctus@users.noreply.github.com>
2021-08-31 21:31:41 +0530

    adding payment model

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.7'
 gem 'devise'
 gem 'jquery-rails'
+gem 'stripe'
 
 
 # Use Redis adapter to run Action Cable in production
---
+++ app/models/payment.rb
@@ -0,0 +1,8 @@
+class Payment < ApplicationRecord
+  attr_accessor :card_number, :card_cvv, :card_expires_month, :card_expires_year
+  belongs_to :user 
+
+  def self.month_options  
+    Date::MONTHNAMES.compact.each_with_index.map{ |name, i|}
+
+end
